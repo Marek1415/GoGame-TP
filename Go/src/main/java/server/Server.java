e32b6a3906c5b949c2cf5c653f2f20d50522782c
Marek1415 <bob@kacper-marek14@wp.pl>
2019-12-02 16:11:08 +0100

    Zrobimy proste gui do testu, potem zorganizujemy na tym lepszym.

---
+++ Go/src/main/java/server/Server.java
@@ -0,0 +1,94 @@
+
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.*;
+
+class SocketServer
+{
+	ServerSocket server = null;
+	Socket client = null;
+	Scanner in = null;
+	PrintWriter out = null;
+	String line = "";
+	int port_number = 4444;
+	SocketServer()
+	{
+		try
+			{
+				server = new ServerSocket(port_number);
+			}
+		catch(IOException e)
+			{
+				System.out.println("Nie można utworzyć serwera na porcie " + port_number);
+				System.exit(-1);
+			}
+	}
+	public void listen()
+	{
+		try
+			{
+				client = server.accept();
+			}
+		catch(IOException e)
+			{
+				System.out.println("Accept failed: 4444");
+				System.exit(-1);
+			}
+		try
+			{
+				in = new Scanner(new InputStreamReader(client.getInputStream()));
+				out = new PrintWriter(client.getOutputStream(), true);
+			}
+		catch(IOException e)
+			{
+				System.out.println("Accept failed: 4444");
+				System.exit(-1);
+			}
+		while(line != null)
+			{
+				try
+					{
+						if(!in.hasNextLine())
+							{
+								break;
+							}
+						line = in.nextLine();
+						System.out.println(line);
+					}
+				catch(Exception e)
+					{
+						System.out.println("Error: problem z inputem");
+						System.exit(-1);
+					}
+			}
+	}
+	protected void finalize()
+	{
+		try
+			{
+				in.close();
+				out.close();
+				client.close();
+				server.close();
+			}
+		catch(IOException e)
+			{
+				System.out.println("Problemy z zamknieciem serwera");
+				System.exit(-1);
+			}
+	}
+}
+
+public class Server
+{
+	public static void main(String[] args)
+	{
+		SocketServer server = new SocketServer();
+		server.listen();
+	}
+}
