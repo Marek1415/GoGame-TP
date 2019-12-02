cd2f90867aed5ef99332db514c6d58633bf86520
Damian Balinski <gumis5151@gmail.com>
2019-12-02 13:57:24 +0100

    FIX BorderPanel, PawnColors

--- Go/src/main/java/client/panels/BoardPanel.java
+++ Go/src/main/java/client/panels/BoardPanel.java
@@ -1,5 +1,6 @@
 package client.panels;
 
+import javax.imageio.ImageIO;
 import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -15,6 +16,7 @@ import static constants.PanelsConstants.*;
 import static constants.PawnColors.*;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -24,6 +26,8 @@ import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
 /*
  * Panel for displaying board.
@@ -69,24 +73,24 @@ implements PawnOperations {
 		//x axis
 		gbc.gridx = 1;
 		gbc.gridy = 0;
-		gbc.weightx = size;
+		//gbc.weightx = size;
 		//gbc.weighty = 1;
-		gbc.weighty = 0.5;
+		//gbc.weighty = 0.5;
 		add(xAxis, gbc);
 		
 		//y axis
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		//gbc.weightx = 1.5;
-		gbc.weightx = 0.5;
-		gbc.weighty = size;
+		//gbc.weightx = 0.5;
+		//gbc.weighty = size;
 		add(yAxis, gbc);
 		
 		//board
 		gbc.gridx = 1;
 		gbc.gridy = 1;
-		gbc.weightx = size;
-		gbc.weighty = size;
+		//gbc.weightx = size;
+		//gbc.weighty = size;
 		add(board, gbc);
 		
 		setVisible(true);
@@ -99,6 +103,9 @@ implements PawnOperations {
 		BoardPanel boardPanel = new BoardPanel();
 		boardPanel.init(7);
 		boardPanel.addPawn(5, PAWN_WHITE);
+		boardPanel.addPawn(15, PAWN_WHITE);
+		boardPanel.addPawn(24, PAWN_BLACK);
+		boardPanel.removePawn(5);
 	}
 	
 	/** Returns the number of pressed button, must by override by parent*/
@@ -145,25 +152,34 @@ implements PawnOperations {
 		
 		/** Adds a pawn with specific color on specific position.*/
 		public void addPawn(int number, int color) {
-			layerButtons.addPawn(number, color);
+			layerGrid.addPawn(number, color);
 		}
 		
 		/** Remove a pawn from specific position.*/
 		public void removePawn(int number) {
-			layerButtons.removePawn(number);
+			layerGrid.removePawn(number);
 		}
 		
 		/*
 		 * Panel for board grid.
 		 */
-		private class LayerGrid extends JPanel {
+		private class LayerGrid extends JPanel
+		implements PawnOperations {
 			
 			int size;
+			int [][] pawns;
+			
+			final Image WHITE;
+			final Image BLACK;
 			
 			public LayerGrid(int newSize) {
 				super();
 				this.size = newSize;
-				//setPreferredSize(DIM_BOARD);
+				pawns = new int[size][size];
+				
+				WHITE = getImage("images/white.png");
+				BLACK = getImage("images/black.png");
+				
 				setBackground(Color.RED);
 				repaint();
 			}
@@ -184,31 +200,59 @@ implements PawnOperations {
 		        //rows
 		        for(int i = 0; i < size; i++)
 		        	g.fillRect(step2, step2 + i*step, length, THICKNESS);
+		        
+		        //pawns
+		        for(int i = 0; i < size; i++)
+		        	for(int j = 0; j < size; j++)
+		        		if(pawns[i][j] == PAWN_WHITE)
+		        			g.drawImage(WHITE, j*step, i*step, null);
+		        		else if(pawns[i][j] == PAWN_BLACK)
+		        			g.drawImage(BLACK, j*step, i*step, null);
 		    }
+			
+			/** Return scaled icon.*/
+			public Image getImage(String path) {
+				
+				Image image = null;
+				try {
+					image = ImageIO.read(new File(path));
+				} catch (IOException e) {}
+
+				return image.getScaledInstance((int)LENGTH_BOARD/size, (int)LENGTH_BOARD/size,  java.awt.Image.SCALE_SMOOTH);
+			}
+			
+			/** Adds a pawn with specific color on specific position.*/
+			public void addPawn(int number, int color) {
+				System.out.println("New pawn!");
+				switch(color) {
+				case PAWN_WHITE: 
+					pawns[number/size][number%size] = PAWN_WHITE;
+					break;
+				case PAWN_BLACK:
+					pawns[number/size][number%size] = PAWN_BLACK;
+					break;
+				}
+				repaint();
+			}
+			
+			/** Remove a pawn from specific position.*/
+			public void removePawn(int number) {
+				System.out.println("Remove pawn!");
+				pawns[number/size][number%size] = NONE;
+			}
 		}
 		
 		/*
 		 * Panel for displaying buttons.
 		 */
-		private class LayerButtons extends JPanel
-		implements PawnOperations {
+		private class LayerButtons extends JPanel {
 			
 			AbstractButton[][] buttons;
-			int size;
-			
-			final ImageIcon WHITE;
-			final ImageIcon BLACK;
 			
 			public LayerButtons(int size) {
 				
 				buttons = new BoardButton[size][size];
 				setLayout(new GridLayout(size,size));
-				this.size = size;
-				
-				WHITE = getScaledImage("/Go/images/white.png");
-				BLACK = getScaledImage("/Go/images/black.png");
-				
-				//setPreferredSize(DIM_BOARD);
 				
 				int k = 0;
 				for(int i = 0; i < size; i++)
@@ -217,34 +261,6 @@ implements PawnOperations {
 						add(buttons[i][j]);
 						k++;
 					}
-			}
-			
-			/** Adds a pawn with specific color on specific position.*/
-			public void addPawn(int number, int color) {
-				System.out.println("New pawn!");
-				switch(color) {
-				case PAWN_WHITE: 
-					//buttons[number/size][number%size].setIcon(WHITE);
-					break;
-				case PAWN_BLACK:
-					//buttons[number/size][number%size].setIcon(BLACK);
-					break;
-				}
-				//setVisible(true);
-				//repaint();
-			}
-			
-			/** Remove a pawn from specific position.*/
-			public void removePawn(int number) {
-				System.out.println("Remove!");
-			}
-			
-			/** Return scaled icon.*/
-			public ImageIcon getScaledImage(String path) {
-				ImageIcon imageIcon = new ImageIcon(path);
-				Image image = imageIcon.getImage();
-				Image newimg = image.getScaledInstance(10, 10,  java.awt.Image.SCALE_SMOOTH);
-				return new ImageIcon(newimg);
 			}
 		}
 		
@@ -271,8 +287,8 @@ implements PawnOperations {
 	    			@Override
 	    			public void mouseExited(MouseEvent e) {
 	    				setBackground(new Color(0,0,0,0));
-	    				//repaint();
 	    				repaintNow(getBounds());
+	    				repaint();
 	    			}
 	    			
 	    			@Override
@@ -325,7 +341,7 @@ implements PawnOperations {
 			setVerticalAlignment(CENTER);
 			setFont(FONT_AXIS);
 			setForeground(COL_AXIS);
-			//setOpaque(true);
+			setPreferredSize(new Dimension(1,1));
 		}
 	}
 }
--- Go/src/main/java/constants/PawnColors.java
+++ Go/src/main/java/constants/PawnColors.java
@@ -5,8 +5,9 @@ package constants;
  * Available colors of pawn.
  */
 public class PawnColors {
-	public final static int PAWN_WHITE = 0;
+	public final static int PAWN_WHITE = -1;
 	public final static int PAWN_BLACK = 1;
+	public final static int NONE = 0;
 	
 	private PawnColors() {}
 }
