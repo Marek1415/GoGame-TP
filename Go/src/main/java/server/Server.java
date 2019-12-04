95abf0a2491eb50f928e974850ca7304360a552f
Marek1415 <bob@kacper-marek14@wp.pl>
2019-12-04 18:47:38 +0100

    Creating backup branch. Started improving readibility of code.

--- Go/src/main/java/server/Server.java
+++ Go/src/main/java/server/Server.java
@@ -1,4 +1,3 @@
-
 package server;
 
 import java.io.IOException;
@@ -129,101 +128,10 @@ class SocketServer
 				System.exit(-1);
 			}
 	}
-}
 
-class ServerThread extends Thread
-{
-	Scanner threadIn = null;
-	PrintWriter threadOut = null;
-	Socket socket = null;
-	String line;
-	ServerThread opponent;
-	int color;
-	ServerThread(Socket socket, int color)
+	static synchronized void removeThread(ServerThread thread)
 	{
-		this.color = color;
-		this.socket = socket;
-		try
-			{
-				threadIn = new Scanner(new InputStreamReader(socket.getInputStream()));
-				threadOut = new PrintWriter(socket.getOutputStream(), true);
-			}
-		catch(IOException e)
-			{
-				System.out.println("Nie można utworzyć wątku na porcie");
-				System.exit(-1);
-			}
-	}
-		
-	public void run()
-	{
-		if(color == 1)
-		{
-			opponent = SocketServer.serverThreads.get(1);
-		}
-		else
-		{
-			opponent = SocketServer.serverThreads.get(0);
-		}
-		System.out.println(this.color + " wątek zadziałał");
-		System.out.println(opponent + " to mój przeciwnik");
-		if(color == 1)
-			{
-				threadOut.println("black");
-			}
-		else
-			{
-				threadOut.println("green");
-			}
-		while(true)
-			{
-				try
-					{
-						try
-							{
-								if(!threadIn.hasNextLine())
-									{
-										break;
-									}
-								line = threadIn.nextLine();
-								String splitString[] = line.split("X|Y");
-								int dimensionsX = Integer.parseInt(splitString[1]);
-								int dimensionsY = Integer.parseInt(splitString[2]);
-								if(SocketServer.game[dimensionsX][dimensionsY] == 0)
-									{
-										SocketServer.game[dimensionsX][dimensionsY] = color;
-										threadOut.println("ok");
-										opponent.threadOut.println(line);
-									}
-							}
-						catch(Exception e)
-							{
-								System.out.println("Problem z inputem w wątku");
-								break;
-							}
-					}
-				catch(Exception e)
-					{
-						System.out.println("Błąd odczytu w wątku");
-						break;
-					}
-			}
-		SocketServer.serverThreads.remove(this);
-		close();
-	}
-	public void close()
-	{
-		try
-			{
-				threadIn.close();
-				threadOut.close();
-				socket.close();
-			}
-		catch(Exception e)
-			{
-				System.out.println("Problemy z zamknięciem wątku");
-				System.exit(-1);
-			}
+		serverThreads.remove(thread);
 	}
 }
 
---
+++ Go/src/main/java/server/ServerThread.java
@@ -0,0 +1,106 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.*;
+
+class ServerThread extends Thread
+{
+	Scanner threadIn = null;
+	PrintWriter threadOut = null;
+	Socket socket = null;
+	String line;
+	ServerThread opponent;
+	int color;
+	ServerThread(Socket socket, int color)
+	{
+		this.color = color;
+		this.socket = socket;
+		try
+			{
+				threadIn = new Scanner(new InputStreamReader(socket.getInputStream()));
+				threadOut = new PrintWriter(socket.getOutputStream(), true);
+			}
+		catch(IOException e)
+			{
+				System.out.println("Nie można utworzyć wątku na porcie");
+				System.exit(-1);
+			}
+	}
+		
+	public void run()
+	{
+		if(color == 1)
+		{
+			opponent = SocketServer.serverThreads.get(1);
+		}
+		else
+		{
+			opponent = SocketServer.serverThreads.get(0);
+		}
+		System.out.println(this.color + " wątek zadziałał");
+		System.out.println(opponent + " to mój przeciwnik");
+		if(color == 1)
+			{
+				threadOut.println("black");
+			}
+		else
+			{
+				threadOut.println("green");
+			}
+		while(true)
+			{
+				try
+					{
+						try
+							{
+								if(!threadIn.hasNextLine())
+									{
+										break;
+									}
+								line = threadIn.nextLine();
+								String splitString[] = line.split("X|Y");
+								int dimensionsX = Integer.parseInt(splitString[1]);
+								int dimensionsY = Integer.parseInt(splitString[2]);
+								if(SocketServer.game[dimensionsX][dimensionsY] == 0)
+									{
+										SocketServer.game[dimensionsX][dimensionsY] = color;
+										threadOut.println("ok");
+										opponent.threadOut.println(line);
+									}
+							}
+						catch(Exception e)
+							{
+								System.out.println("Problem z inputem w wątku");
+								break;
+							}
+					}
+				catch(Exception e)
+					{
+						System.out.println("Błąd odczytu w wątku");
+						break;
+					}
+			}
+		SocketServer.removeThread(this);
+		close();
+	}
+	public void close()
+	{
+		try
+			{
+				threadIn.close();
+				threadOut.close();
+				socket.close();
+			}
+		catch(Exception e)
+			{
+				System.out.println("Problemy z zamknięciem wątku");
+				System.exit(-1);
+			}
+	}
+}
