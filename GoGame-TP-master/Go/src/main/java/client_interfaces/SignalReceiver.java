2cdf169db35205803d11c37e9fbe5e21bbdce645
Marek1415 <bob@kacper-marek14@wp.pl>
2019-12-08 22:21:18 +0100

    removed duplicated directory

--- GoGame-TP-master/Go/src/main/java/client_interfaces/SignalReceiver.java
+++
@@ -1,7 +0,0 @@
-package client_interfaces;
-
-/** Class has to implement the method to receive the signal.*/
-public interface SignalReceiver {
-
-	public void recSignal(String signal);
-}
--- GoGame-TP-master/Go/src/main/java/client_interfaces/SignalSender.java
+++
@@ -1,7 +0,0 @@
-package client_interfaces;
-
-/** Class has to implement the method for receive the signal.*/
-public interface SignalSender {
-
-	public void sendSignal(String signal);
-}
