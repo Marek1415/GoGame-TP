533574e4c9e972780cbd04033f2222b00036760b
damianbalinski <damianbalinski99@gmail.com>
2021-03-29 00:26:14 +0200

    move project to IntelliJ

--- Go/src/main/java/client_interfaces/PawnOperations.java
+++
@@ -1,6 +0,0 @@
-package client_interfaces;
-
-public interface PawnOperations {
-	public void addPawn(int number, int color);
-	public void removePawn(int number);
-}
--- Go/src/main/java/constants/BoardSizes.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class BoardSizes {
-	
-	public static final int [] SIZES = new int[] {9,13,19,21,25,40};
-	
-	private BoardSizes() {}
-}
